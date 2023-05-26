915d8e8559e6eadacfe32d93f73c205e612624a4
Leespire <spreadyourwings1993@naver.com>
2023-05-26 15:37:57 +0900

    sixth commit

--- system/system_server.c
+++ system/system_server.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 #include <sys/prctl.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+static int toy_timer = 0;
+
+int posix_sleep_ms(unsigned int timeout_ms)
+{
+    struct timespec sleep_time;
+
+    sleep_time.tv_sec = timeout_ms / MILLISEC_PER_SECOND;
+    sleep_time.tv_nsec = (timeout_ms % MILLISEC_PER_SECOND) * (NANOSEC_PER_USEC * USEC_PER_MILLISEC);
+
+    return nanosleep(&sleep_time, NULL);
+}
+
 int system_server()
 {
+    struct itimerspec ts;
+    struct sigaction  sa;
+    struct sigevent   sev;
+    timer_t *tidlist;
+
     printf("나 system_server 프로세스!\n");
 
+    /* 5초 타이머를 만들어 봅시다. */
+
     while (1) {
-        sleep(1);
+        posix_sleep_ms(5000);
     }
 
     return 0;
